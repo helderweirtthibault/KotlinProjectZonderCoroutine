d271e988ab9abcd30d450cdc0110d619e2314464
thibault.helderweirt@student.hogent.be <thibault.helderweirt@student.hogent.be>
2021-12-13 17:24:06 +0100

    Initialisatie wijziging Google Maps

--- app/src/main/java/com/example/kotlinperformantieapp/MapsActivity.kt
+++ app/src/main/java/com/example/kotlinperformantieapp/MapsActivity.kt
@@ -37,14 +37,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
-        locationManager = getSystemService(LOCATION_SERVICE) as LocationManager?
 
-            try {
-                // Request location updates
-                locationManager?.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0L, 0f, locationListener)
-            } catch(ex: SecurityException) {
-                Log.d("myTag", "Security Exception, no location available")
-            }
     }
 
     fun changeCourse(lat: String, long: String){
@@ -67,9 +60,19 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        locationManager = getSystemService(LOCATION_SERVICE) as LocationManager?
+
+        try {
+            // Request location updates
+            locationManager?.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0L, 0f, locationListener)
+        } catch(ex: SecurityException) {
+            // Add a marker in Sydney and move the camera
+            val sydney = LatLng(-34.0, 151.0)
+            mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+            Log.d("myTag", "Security Exception, no location available")
+        }
+
+
     }
 }
